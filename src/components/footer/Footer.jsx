0cc82b28c7090a4f38e11b1319a0667e0c04f236
thekage.eth <m.siddik56@hotmail.com>
2023-12-12 05:30:40 +0000

    finish only 3d element left for footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { FaSnapchatGhost } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-600 h-[200px] text-center border-t p-8  mt-16 ">
+    <footer className="bg-green-600 h-[200px] text-center border-t p-8  mt-16  flex flex-col ">
       <p>
         &copy; Chef Zozo 2023 | Developed By
         <a
           className="hover:bg-black hover:text-white"
           href="https://Phantom3.dev"
         >
-          {" "}
           Phantom3.dev
         </a>
       </p>
+      <div className="flex  justify-center">
+        <a href="http://instagram.com/zaenbnabil/">
+          <FaInstagram className="bg-pink-500 w-[25px] h-[25px] m-1 mt-5" />
+        </a>
+
+        <a href="https://t.snapchat.com/wdlh7kWD">
+          {" "}
+          <FaSnapchatGhost className=" bg-yellow-500 text-gray-300 w-[25px] h-[25px] m-1 mt-5" />
+        </a>
+      </div>
     </footer>
   );
 };
